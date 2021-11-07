9ad27643371c82ee61b41e9f7e4f25e37ed510d1
Songycs <doong117@gmail.com>
2021-11-07 22:10:17 +0900

    feat: react voca json_server & api 1107

--- REACTJS/react_voca/voca/src/App.js
+++ REACTJS/react_voca/voca/src/App.js
@@ -3,6 +3,8 @@ import DayList from "./components/DayList";
 import Day from "./components/Day";
 import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList/>}/>
           <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/create_word" element={<CreateWord/>}/>
           <Route element={<EmptyPage/>}/>
         </Routes>
       </div>
--- REACTJS/react_voca/voca/src/components/Day.js
+++ REACTJS/react_voca/voca/src/components/Day.js
@@ -1,30 +1,21 @@
 import dummy from "../DB/data.json"
 import { useParams } from "react-router-dom";
-
+import Word from "./Word"
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 export default function Day(){
 
     const day = useParams().day;
-    const wordList = dummy.words.filter(word=>
-        word.day===Number(day)
-    );
-    console.log(wordList)
+    const words = useFetch(`http://localhost:3001/words?day=${day}`)
+    
+
     return (
     <>
         <h2>Day {day}</h2>
         <table>
             <tbody>
-                {wordList.map(word=>(
-                    <tr key={word.id}>
-                        <td>
-                            <input type="checkbox"/>
-                        </td>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                        <td>
-                            <button> 뜻 보기 </button>
-                            <button className = "btn_del"> 삭제하기 </button>
-                        </td>
-                    </tr>
+                {words.map(word=>(
+                    <Word word={word} key={word.id}/>
                 ))}
             </tbody>
         </table>    
